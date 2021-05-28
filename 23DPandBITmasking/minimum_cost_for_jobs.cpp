382c801f35602db9441a22816772d40f8d79af2b
Avinesh <avineshpandey2101@gmail.com>
2021-05-28 17:21:48 +0530

    mincostjb

--- 23DPandBITmasking/minimum_cost_for_jobs.cpp
+++ 23DPandBITmasking/minimum_cost_for_jobs.cpp
@@ -2,12 +2,36 @@
 using namespace std;
 
 //-----------------------------------iterative------------------------------------
-int iterativeMinCost()
+int iterativeMinCost(int cost[][4],int n)
 {
-    
-
-
-
+    int *dp=new int [1<<n];
+    for(int i=0; i<(1<<n); i++)
+    {
+        dp[i]=INT_MAX;
+    }
+    dp[0]=0;
+    for(int mask=0; mask<(1<<n)-1; mask++)
+    {
+        //now we have to count the number of set bits
+        int temp=mask;
+        int k=0;
+        while(temp>0)
+        {
+            if(temp%2==1)
+            {
+                k++;
+            }
+            temp/=2;
+        }
+        for(int j=0; j<n; j++)
+        {
+            if(!(mask&(1<<j)))
+            {
+                dp[mask|(1<<j)]=min(dp[mask|(1<<j)], dp[mask]+cost[k][j]);
+            }
+        }
+    }
+    return dp[(1<<n)-1];
 }
 
 
