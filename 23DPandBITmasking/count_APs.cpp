b243ab79c68d5b6d12c800c3ed4c168751911fef
Avinesh <avineshpandey2101@gmail.com>
2021-06-04 13:09:03 +0530

    added qus

--- 23DPandBITmasking/count_APs.cpp
+++ 23DPandBITmasking/count_APs.cpp
@@ -31,13 +31,31 @@ int numofAP(int *arr, int n){
     Return all possible AP subsequences formed from the given integer array. You donot need to take input or print anything.
     Just return the ans.
     */
-        int ans = n+1;
-        int**dp = new int[1000];
-        for(int i=0;i<n;i++)
+      int ans=n+1;
+    unordered_map<int, int>*map=new unordered_map<int, int>[n];
+    //here this map will store that how many APs are their for this particular index with some particular common difference y
+    for(int i=0; i<n-1; i++)
+    {
+        for(int j=i+1; j<n; j++)
         {
-            dp[i] = new int[1000];
-            
+            int diff=arr[j]-arr[i];
+            map[i/*on ith index*/][diff/*with this diff*/]+=1/*I have got one more AP*/;
+            ans=(ans+1)%m;
         }
+    }
+    for(int i=n-3; i>=0; i--)
+    {
+        for(int j=i+1; j<n; j++)
+        {
+            int diff=arr[j]-arr[i];
+            map[i][diff]=(map[i][diff]%m+map[j][diff]%m)%m;
+            ans=(ans%m+map[j][diff]%m)%m;
+        }
+    }
+    delete[]map;
+    return ans;
+
+    
 }
 
 int main(){
